1e1a567d19cf25406fbe3687020950a73517eba7
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2021-10-19 09:42:24 +0200

    go lang profile service

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -8,6 +8,7 @@ import { GraphQLGatewayModule } from '@nestjs/graphql';
         serviceList: [
           { name: 'posts', url: 'http://localhost:3000/graphql' },
           { name: 'users', url: 'http://localhost:3001/graphql' },
+          { name: 'profile', url: 'http://localhost:4002/query' },
         ],
       },
     }),
